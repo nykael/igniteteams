5181e919faee491c2848510b5b1f51e215229bf0
nykael <nykaelccardial@gmail.com>
2022-09-26 17:51:43 -0300

    logic to list players

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,10 +1,17 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AppError } from "@utils/AppError";
 import { groupsGetAll } from "./groupGetAll";
-import { GROUP_COLLECTION } from "./storageConfig";
+import { GROUP_COLLECTION } from "../storageConfig";
 
 export async function groupCreate(newGroup: string) {
     try {
         const storagedGroups = await groupsGetAll();
+
+        const groupAlreadyExists = storagedGroups.includes(newGroup)
+
+        if(groupAlreadyExists) {
+            throw new AppError('Já existe um grupo cadastrado com esse nome')
+        }
 
         const storage = JSON.stringify([...storagedGroups, newGroup])
 
